bc455cee6146677ff75cc16be67345bf42246406
Daniel Hristov <dhristov06@gmail.com>
2025-11-24 18:32:53 +0200

    brought glfw and glad

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,44 @@
-#include <iostream>
 #include <print>
-#include "AudioPlayer.h"
-#include "LibraryManager.h"
-#include "tinyfiledialogs.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 int main() {
-	LibraryManager manager;
-	manager.import();
-
-	AudioPlayer player;
-	std::string cmd;
-	std::string song;
-
-	std::getline(std::cin, song);
-	auto& filepath = manager.mSongs[std::stoi(song) - 1];
-
-	while (true) {
-		int i = 0;
-		for (const auto& entry : manager.mSongs) {
-			std::println("{}: {}", i + 1, entry.stem().string());
-			i++;
-		}
-
-		std::getline(std::cin, cmd);
-
-		switch (cmd[0]) {
-			case 'q':
-				return 0;
-			case 'x':
-				player.play(filepath.string());
-				break;
-			case 's':
-				player.stop();
-				break;
-			case 'p':
-				player.pause();
-				break;
-			case 'r':
-				player.resume();
-				break;
-			default:
-				std::cout << "Unkown command\n";
-				break;
-		}
+	if (!glfwInit()) {
+		std::println("Could not initialize GLFW.\n");
+		return -1;
 	}
 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#if __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+	
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "Audio Player", NULL, NULL);
+	if (!window) {
+		std::println("Failed to create a window.\n");
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	glfwSetFramebufferSizeCallback(window, [](GLFWwindow* window, int width, int height) {
+		glViewport(0, 0, width, height);
+	});
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::println("Could not initialize GLAD.\n");
+		return -1;
+	}
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
 	return 0;
 }
