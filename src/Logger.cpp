eddbb74a7148a86aee407855e51e76616e3ae30a
Daniel Hristov <dhristov06@gmail.com>
2026-04-27 00:21:23 +0300

    Dynamic log window name

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -6,9 +6,16 @@ void Logger::drawWindow() {
 	if (mMessage.empty()) return;
 
 	if (mWindowOpen) {
+		const char* windowName = "";
+
+		switch (mLevel) {
+		case Level::Error: windowName = "Error"; break;
+		case Level::Warning: windowName = "Warning"; break;
+		}
+
 		ImGui::SetNextWindowSize(ImVec2(350, 120));
 
-		ImGui::Begin("Message", &mWindowOpen, ImGuiWindowFlags_NoDocking | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize);
+		ImGui::Begin(windowName, &mWindowOpen, ImGuiWindowFlags_NoDocking | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize);
 		const ImVec2 widgetSpacing = ImVec2(0.0f, 8.0f);
 
 		ImGui::Text(utils::toUtf8(mMessage).c_str());
